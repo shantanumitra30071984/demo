4288104c099473eb81ffaf7e1a0be6f393c020e3
Shantanu <shantanu.mitra4@gmail.com>
2026-05-31 13:53:30 +0530

    pom version changed, added testcase

---
+++ src/test/java/com/jenkins/demo/controller/TestControllerTest.java
@@ -0,0 +1,34 @@
+package com.jenkins.demo.controller;
+
+
+import org.junit.jupiter.api.Test;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.autoconfigure.web.servlet.WebMvcTest;
+import org.springframework.test.web.servlet.MockMvc;
+
+import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.get;
+import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.status;
+import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.content;
+
+@WebMvcTest(TestController.class)
+public class TestControllerTest {
+
+    @Autowired
+    private MockMvc mockMvc;
+
+    @Test
+    public void testGreet() throws Exception {
+        mockMvc.perform(get("/test/greet"))
+                .andExpect(status().isOk())
+                .andExpect(content().string("Hello World"));
+    }
+
+    @Test
+    public void testGreetReturnsHelloWorld() throws Exception {
+        mockMvc.perform(get("/test/greet"))
+                .andExpect(status().isOk())
+                .andExpect(content().contentType("text/plain;charset=UTF-8"))
+                .andExpect(content().string("Hello World"));
+    }
+}
+
